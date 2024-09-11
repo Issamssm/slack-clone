e2b3ab4208de0563e225b82ac051701e9b2f12f5
Issam_lizame <lizameissam16@gmail.com>
2024-09-11 19:40:08 +0100

    "Added messages table to schema, implemented create message API, and updated chat input component to use new API"

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -80,7 +80,16 @@ const Editor = ({
                         enter: {
                             key: "Enter",
                             handler: () => {
-                                return;
+                                const text = quill.getText();
+                                const addedImage = imageElementRef.current?.files?.[0] || null;
+
+                                const isEmpty = !addedImage && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+
+                                if (isEmpty) return;
+
+                                const body = JSON.stringify(quill.getContents());
+                                submitRef.current?.({ body, image: addedImage })
+
                             }
                         },
                         shift_enter: {
@@ -124,7 +133,7 @@ const Editor = ({
         };
     }, [innerRef]);
 
-    const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+    const isEmpty = !image && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
     const togglToolbar = () => {
         setIsToolbarVisible((current) => !current);
@@ -150,7 +159,10 @@ const Editor = ({
                 onChange={(e) => setImage(e.target.files![0])}
                 className="hidden"
             />
-            <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
+            <div className={cn(
+                "flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white",
+                disabled && "opacity-50"
+            )}>
                 <div ref={containerRef} className="h-full ql-custom" />
                 {!!image && (
                     <div className="p-2">
@@ -215,7 +227,7 @@ const Editor = ({
                             <Button
                                 disabled={disabled}
                                 size={"sm"}
-                                onClick={() => { }}
+                                onClick={onCancel}
                                 variant={"outline"}
                             >
                                 Cancel
@@ -223,7 +235,12 @@ const Editor = ({
                             <Button
                                 disabled={disabled || isEmpty}
                                 size={"sm"}
-                                onClick={() => { }}
+                                onClick={() => {
+                                    onSubmit({
+                                        body: JSON.stringify(quillref.current?.getContents()),
+                                        image
+                                    })
+                                }}                                
                                 className="bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
                             >
                                 Save
@@ -234,7 +251,12 @@ const Editor = ({
                         <Button
                             disabled={disabled || isEmpty}
                             size={"iconSm"}
-                            onClick={() => { }}
+                            onClick={() => {
+                                onSubmit({
+                                    body: JSON.stringify(quillref.current?.getContents()),
+                                    image
+                                })
+                            }}
                             className={cn(
                                 "ml-auto",
                                 isEmpty ? " bg-white hover:bg-white text-muted-foreground" : " bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
